061c4e15be49b4830a59863edd80493d639c00a2
carlov20 <carlov20@users.noreply.github.com>
2022-09-16 15:23:49 +0100

    WFP-1394 updated script to handle new version of spring boot plugin and run a small gradle task for trivy (#30)

--- scripts/dps-spring-boot-gradle-plugin/upgrade-latest.bash
+++ scripts/dps-spring-boot-gradle-plugin/upgrade-latest.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed -i '' -e 's/spring-boot") version "4.[0-3].[0-9]*\(-beta\)*/spring-boot") version "3.3.12/' build.gradl*
+sed -i '' -e 's/spring-boot") version "[0-9].[0-9].[0-9]*\(-beta\)*/spring-boot") version "4.5.1-beta/' build.gradl*
 
 # Or multiple updates...
 #sed -i '' -e 's/spring-boot") version "3.[0-3].[0-9]\(-beta\)*/spring-boot") version "3.3.9/' \
@@ -8,3 +8,6 @@ sed -i '' -e 's/spring-boot") version "4.[0-3].[0-9]*\(-beta\)*/spring-boot") ve
 
 # Holding off doing the gradle wrapper until we resolve: DT-2653
 #./gradlew wrapper --gradle-version=7.2 --distribution-type=bin
+
+# run a small gradle task to generate trivy ignore
+./gradlew ktlintFormat
