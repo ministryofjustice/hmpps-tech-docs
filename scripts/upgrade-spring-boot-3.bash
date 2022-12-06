b7b435259c23ca9ac87cc8b114c4c7dac5bc3829
Peter Phillips <peter@greenthistle.co.uk>
2022-12-06 11:58:19 +0000

    SDI-477: ✨ Stab at spring boot 3 upgrade script

---
+++ scripts/upgrade-spring-boot-3.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+./gradlew wrapper --gradle-version=7.6 --distribution-type=bin
+
+./gradlew useLatestVersions
+
+find . -name '*.kt' -exec sed -i \
+ -e 's/import javax.servlet/import jakarta.servlet/' \
+ -e 's/import javax.validation/import jakarta.validation/' \
+ -e 's/import javax.transaction/import jakarta.transaction/' \
+ -e 's/import javax.persistence/import jakarta.persistence/' \
+ -e 's/EnableGlobalMethodSecurity/EnableMethodSecurity/' \
+ -e 's/authorizeRequests/authorizeHttpRequests/' \
+ -e 's/antMatchers/requestMatchers/' \
+ -e 's/import org.hibernate.annotations.Type/import org.hibernate.type.YesNoConverter\nimport jakarta.persistence.Convert/' \
+ -e 's/@Type(type = "yes_no")/@Convert(converter = YesNoConverter::class)/' \
+ -e 's/antMatchers/requestMatchers/' \
+ -e 's/import org.springframework.boot.web.server.LocalServerPort/import org.springframework.boot.test.web.server.LocalServerPort/' \
+ {} \;
+
+find . -name '*.yml' -exec sed -i \
+ -e 's/PostgreSQL10Dialect/PostgreSQLDialect/' \
+ {} \;
+
+
+sed -i 's/com.vladmihalcea:hibernate-types-52/com.vladmihalcea:hibernate-types-60/' build.gradle.kts
